9de4bdba54d5ffb4ff561aaae588a97641892b1b
newdots <114389694+newdots@users.noreply.github.com>
2022-12-06 13:21:14 +0800

    update

--- Package.swift
+++ Package.swift
@@ -10,7 +10,7 @@ let package = Package(
   targets: [
     .binaryTarget(
       name: "ClashKit",
-      url: "https://github.com/AppleClash/ClashKit/releases/download/3.4.0/ClashKit.xcframework.zip",
+      url: "https://github.com/newdots/ClashKit/releases/download/v3.4.0/ClashKit.xcframework.zip",
       checksum: "0d3ea18dac553c071c6f526062c423aa0e3e2868ca4da9b0c2606bb758a418f6"
     )
   ]
